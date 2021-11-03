c4cf455cc8a2ed3bf031badf7636c84f9093a91d
pablo-maff <pablo.maff.076@gmail.com>
2021-11-04 00:29:47 +0100

    Exercise 1.3 DONE

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,45 +1,66 @@
 /*
-Exercise 1.2
+Exercise 1.3
 
-Refactor the Content component so that it does not render any names
-of parts or their number of exercises by itself. Instead it only
-renders three Part components of which each renders the name and
-number of exercises of one part.
+Let's move forward to using objects in our application.
+Modify the variable definitions of the App component as follows
+and also refactor the application so that it still works:
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  return (
+    <div>
+      ...
+    </div>
+  )
+}
 */
 
 import React from 'react'
 
-const part1 = 'Fundamentals of React'
-const exercises1 = 10
-const part2 = 'Using props to pass data'
-const exercises2 = 7
-const part3 = 'State of a component'
-const exercises3 = 14
-
 const Header = (props) => <h1>{props.title}</h1>
 
-const Part = (props) => <p>{props.part} {props.exercise}</p>
-
-const Content = () => {
-  return (
-    <>
-      <Part part={part1} exercise={exercises1}/>
-      <Part part={part2} exercise={exercises2}/>
-      <Part part={part3} exercise={exercises3}/>
-    </>
-  ) 
+const Part = (props) => {
+  return <p>{props.part.name} {props.part.exercises}</p>
 }
-
-const Total = (props) => <p>Number of exercises {props.sum}</p>
+const Total = (props) => {
+  return <p>Number of exercises {props.sum}</p>
+}
 
 const App = () => {
   const course = 'Half Stack Application Development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <>
       <Header title={course} />
-      <Content />
-      <Total sum={exercises1 + exercises2 + exercises3} />
+      <Part part={part1} />
+      <Part part={part2} />
+      <Part part={part3} />
+      <Total sum={part1.exercises + part2.exercises + part3.exercises}/>
     </>
   )
 }
