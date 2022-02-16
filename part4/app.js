65202d70d750529845a612edb0818e21ac705a90
pablo-maff <pablo.maff.076@gmail.com>
2022-02-16 11:54:19 +0100

    Middleware added; DB output arranged. Blog list Step 2 (Ex 4.2)

--- part4/app.js
+++ part4/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
 const { info, error } = require('./utils/logger')
 const mongoose = require('mongoose')
 
@@ -18,7 +19,11 @@ mongoose.connect(MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
