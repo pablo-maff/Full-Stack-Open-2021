52f8c14a6ad3917d42834c6517ed21d33ae522f9
pablo-maff <pablo.maff.076@gmail.com>
2022-03-01 14:15:22 +0100

    5.7 in progress. Check useRef to display user name on blog details

--- part5/src/components/Blog.js
+++ part5/src/components/Blog.js
@@ -1,7 +1,47 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} from {blog.author} in {blog.url}
-  </div>  
-)
+import { useState } from "react"
+import Button from "./Button"
+import Togglable from "./Togglable"
+
+const blogStyle = {
+  paddingTop: 10,
+  paddingLeft: 2,
+  border: 'solid',
+  borderWidth: 1,
+  marginBottom: 5
+}
+
+const Blog = ({ blog }) => { 
+  const [view, setView] = useState(false)
+
+  const toggleView = () => {
+     setView(!view)
+  }
+  console.log(view);
+
+  return (
+    <div style={blogStyle}>
+      {view === false ?
+        <>
+          {blog.title} {blog.author}
+          <Button onClick={toggleView} text='View' />
+        </>
+        : 
+        <>
+          <div>
+            <p>{blog.title} {blog.author}</p> 
+            <p>{blog.url}</p>
+            <p>{blog.likes}</p>
+            <p></p>
+            <Button onClick={toggleView} text='Hide' />
+          </div>
+        </>
+      }
+      {/* {view ? 
+        <Button onClick={toggleButton} text='hide' />
+        : <Button onClick={toggleButton} text='view' />
+        {blog.title} {blog.author}
+      } */}
+    </div>
+  )}
 
 export default Blog
