6310648b5c809bfc12e9a8d23ee6cda6e78afce3
pablo-maff <pablo.maff.076@gmail.com>
2022-03-05 11:52:37 +0100

    Blog list tests Step 2 (5.14)

--- part5/src/components/Blog.test.js
+++ part5/src/components/Blog.test.js
@@ -1,22 +1,44 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('renders only title and author', () => {
-  const blog = {
-    title: 'Component testing is done with react-testing-library',
-    author: 'Mr Tester',
-    likes: 23,
-    url: 'https://example.com'
-  }
+describe('<Blog />', () => {
+  let container
+  let div
 
-  const { container } = render(<Blog blog={blog}/>)
+  beforeEach(() => {
+    const blog = {
+      title: 'Component testing is done with react-testing-library',
+      author: 'Mr Tester',
+      likes: 23,
+      url: 'https://example.com',
+      user: {
+        name: 'Pablo Maffioli'
+      }
+    }
+    const user = {
+      name: 'Pablo Maffioli'
+    }
 
-  const div = container.querySelector('.blog')
+    const mockHandler = jest.fn()
+    container = render(<Blog blog={blog} user={user} onClick={mockHandler} />).container
+    div = container.querySelector('.blog')
+  })
 
-  expect(div).toHaveTextContent('Component testing is done with react-testing-library')
-  expect(div).toHaveTextContent('Mr Tester')
-  expect(div).not.toHaveTextContent('23')
-  expect(div).not.toHaveTextContent('https://example.com')
+  test('renders only title and author', () => {
+    expect(div).toHaveTextContent('Component testing is done with react-testing-library')
+    expect(div).toHaveTextContent('Mr Tester')
+    expect(div).not.toHaveTextContent('23')
+    expect(div).not.toHaveTextContent('https://example.com')
+  })
+
+  test('url and likes are shown when pressing the view button', () => {
+    const button = screen.getByText('View')
+    userEvent.click(button)
+
+    expect(div).toHaveTextContent('23')
+    expect(div).toHaveTextContent('https://example.com')
+  })
 })
