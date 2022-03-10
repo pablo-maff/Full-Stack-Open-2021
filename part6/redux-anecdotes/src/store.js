539db744734c67456655d5eaff44571c48769541
pablo-maff <pablo.maff.076@gmail.com>
2022-03-10 21:08:49 +0100

    Anecdotes and the backend Step 1 (Ex 6.13)

--- part6/redux-anecdotes/src/store.js
+++ part6/redux-anecdotes/src/store.js
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import anecdoteReducer from "./reducers/anecdoteReducer";
 import notificationReducer from "./reducers/notificationReducer";
 import filterReducer from "./reducers/filterReducer";
+import anecdoteService from './services/anecdotes'
 
 const store = configureStore({
   reducer: {
@@ -11,6 +12,6 @@ const store = configureStore({
   }
 })
 
-//console.log('store', store.getState());
+console.log('store', store.getState());
 
 export default store
