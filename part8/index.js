aeb9242feaa80d70f5d62e0666cc50606d9e1dc5
pablo-maff <pablo.maff.076@gmail.com>
2022-05-11 01:42:44 +0200

    Ex 8.3

--- part8/index.js
+++ part8/index.js
@@ -89,6 +89,12 @@ let books = [
 ]
 
 const typeDefs = gql`
+  type Author {
+    name: String!
+    id: ID!
+    born: Int
+    bookCount: Int
+  }
 
   type Book {
     title: String!
@@ -99,9 +105,10 @@ const typeDefs = gql`
   }
 
   type Query {
-    bookCount: Int!
+    bookCount: Int
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
@@ -110,7 +117,11 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: () => books,
+    allAuthors: () => authors
   },
+  Author: {
+    bookCount: (root, args) => books.filter(b => b.author === root.name).length
+  }
 }
 
 const server = new ApolloServer({
